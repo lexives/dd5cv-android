80e54b9e08d2e084ad16612c9da91f0b047ecf9d
lexives <lexives96@gmail.com>
2021-04-23 12:42:08 -0500

    fix bug with character id

--- app/src/main/java/com/delarax/dd5cv/models/characters/Character.kt
+++ app/src/main/java/com/delarax/dd5cv/models/characters/Character.kt
@@ -1,8 +1,10 @@
 package com.delarax.dd5cv.models.characters
 
+import com.google.gson.annotations.SerializedName
 import java.util.*
 
 data class Character (
+    @SerializedName("_id")
     val id: String = UUID.randomUUID().toString(),
     val name: String? = null,
     val classes: List<CharacterClassLevel> = listOf()
